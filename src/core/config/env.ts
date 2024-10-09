1951f29a73f3151e6c3d594aa84656f168c5b5b4
kravetsmikhail <kravetsmihail@mail.ru>
2024-10-09 16:58:32 +0300

    Подключен логгер

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -1,16 +1,15 @@
 require("dotenv").config()
-import { normalizePort } from "normalize-port"
 
 export class envs {
-    public static version = process.env.VERSION
-	public static nodeEnv = process.env.NODE_ENV
-	public static host = process.env.HOST || "localhost"
-	public static port = normalizePort(process.env.PORT || '7117')
-	public static dbName = process.env.DB_NAME
-	public static dbHost = process.env.DB_HOST
-	public static dbPort = process.env.DB_PORT || '5432'
-	public static dbUser = process.env.DB_USER
-	public static dbPass = process.env.DB_PAS
-	public static dbSchema = process.env.DB_SCHEMA
-    public static apiPrefix = process.env.API_PREFIX || '123'
+    public static version = <string>process.env.VERSION
+	public static nodeEnv = <string>process.env.NODE_ENV
+	public static host = <string>process.env.HOST || "localhost"
+	public static port = <string>process.env.PORT || '7117'
+	public static dbName = <string>process.env.DB_NAME
+	public static dbHost = <string>process.env.DB_HOST
+	public static dbPort = <string>process.env.DB_PORT || '5432'
+	public static dbUser = <string>process.env.DB_USER
+	public static dbPass = <string>process.env.DB_PAS
+	public static dbSchema = <string>process.env.DB_SCHEMA
+    public static apiPrefix = <string>process.env.API_PREFIX || '123'
 }
--- src/server.ts
+++ src/server.ts
@@ -6,16 +6,18 @@ import { ONE_HUNDRED, ONE_THOUSAND, SIXTY } from './core/constants/constatnts'
 import { HttpCode } from './core/constants/httpcodes'
 import { ErrorMiddleware } from './presentation/middlewares/errors/error.middleware'
 import { AppError } from './core/errors/custom.error'
+import { Logger } from './core/logger/logger'
 
 interface ServerOptions {
-    port: number
+    port: string
     routes: Router
     apiPrefix: string
 }
 
 export class Server {
     private readonly app = express()
-    private readonly port: number
+    private readonly logger = new Logger()
+    private readonly port: string
     private readonly routes: Router
     private readonly apiPrefix: string
 
@@ -31,6 +33,7 @@ export class Server {
         this.app.use(express.json()) // parse json in request body (allow raw)  
         this.app.use(express.urlencoded({ extended: true })) // allow x-www-form-urlencoded  
         this.app.use(compression())
+        this.app.use(this.logger.getRequestLogger())
         //  limit repeated requests to public APIs  
         this.app.use(
             rateLimit({
@@ -59,10 +62,10 @@ export class Server {
         this.app.use(this.apiPrefix, this.routes)
 
         // Test rest api  
-        this.app.get('/', (_req: Request, res: Response) => {
+        this.app.get('/', (_req: Request, res: Response)  => {
             return res.status(HttpCode.OK).send({
                 message: `Welcome to Initial API! n Endpoints available at http://localhost:${this.port}/`
-            })
+            }) as any
         })
 
         //* Handle not found routes in /api/v1/* (only if 'Public content folder' is not available)  
@@ -70,9 +73,11 @@ export class Server {
             next(AppError.notFound(`Cant find ${req.originalUrl} on this server!`));
         })
 
-        // Handle errors middleware  
-        this.routes.use(ErrorMiddleware.handleError);
+        this.app.use(this.logger.getRequestErrorLogger())
 
+        // Handle errors middleware  
+        this.routes.use(ErrorMiddleware.handleError)
+        
         this.app.listen(this.port, () => {
             console.log(`Server running on port ${this.port}...`)
         })
